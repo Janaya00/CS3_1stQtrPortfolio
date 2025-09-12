65ee3cc839f04e1384a755ed7e63f9c6b4c59fae
Janaya00 <b2029jcpagdilao@pshs.edu.ph>
2025-09-12 16:11:27 +0800

    yes

--- yes.js
+++ yes.js
@@ -1,4 +1,26 @@
-const square = function(a) {
-  return a * a;
+
+
+let greet = function(name){
+  console.log('greetings dear ' + name)
 };
 
+greet('micay')
+
+//*greetings function
+
+
+let squared = function( value){
+  
+  return value * value
+};
+
+console.log(squared(5));
+
+//*squared function
+
+setTimeout(greet('rb'), 4000);
+//* setTimeout -- function in a function
+
+greetrb = name => console.log('greetings dear ' + name);
+greetrb('rb')
+//* arrow function
